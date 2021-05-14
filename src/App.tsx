f5805a57474efd0be3bc4f2fc5ec5578607c9dff
Velten Heyn <github@valoric.de>
2021-05-14 13:40:38 +0200

    include types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./styles.css";
 
+import { AtomicProps } from "@fower/types";
+//import { Box } from "@fower/react";
+
+declare module "react" {
+  interface HTMLAttributes<T> extends AtomicProps {}
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div toCenter></div>
     </div>
   );
 }
